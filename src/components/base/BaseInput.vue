b3c5457476eed2749e489d55aa2521f0ffda5e25
Igor Brel <igorbrel1983@gmail.com>
2023-03-06 15:50:38 +0300

    add base input component

---
+++ src/components/base/BaseInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input
+      class="input"
+      @keydown.enter="updateValue"
+      @input="updateValue"
+      @blur="updateValue"
+      :type="type"
+      :value="modelValue"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from "vue";
+
+interface Props {
+  modelValue: string;
+  type: string;
+}
+
+const props = defineProps<Props>();
+
+const emit = defineEmits(["update:modelValue"]);
+
+const updateValue = (event) => {
+  emit("update:modelValue", event.target.value);
+};
+</script>
