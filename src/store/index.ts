e4d7ced4212de8d6592861619dbd489ae9b7fb4a
Igor Brel <igorbrel1983@gmail.com>
2023-03-07 16:05:33 +0300

    create store for auth form

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,30 @@
 import { defineStore } from "pinia";
+import { useRouter } from "vue-router";
+
+export const useStore = defineStore({
+  id: "store",
+
+  state: () => ({
+    email: "",
+    password: "",
+    confirm: "",
+    router: useRouter(),
+  }),
+
+  getters: {
+    currentRoute(state) {
+      return state.router.currentRoute.path;
+    },
+    authForm() {
+      const arrOfInputs = [
+        { model: "email", placeholder: "email", type: "email" },
+        { model: "password", placeholder: "password", type: "password" },
+        { model: "confirm", placeholder: "confirm password", type: "password" },
+      ];
+      return `${this.currentRoute}` === "/register"
+        ? arrOfInputs
+        : arrOfInputs.slice(0, 2);
+    },
+  },
+  actions: {},
+});
