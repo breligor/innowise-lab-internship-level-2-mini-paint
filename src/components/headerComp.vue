1eec7ebad7969de522d709a13c2ff8dab5f81ae0
breligor <77774848+breligor@users.noreply.github.com>
2023-03-12 15:28:44 +0300

    add signOutBtn component to header

--- src/components/headerComp.vue
+++ src/components/headerComp.vue
@@ -1,26 +1,37 @@
 <template>
-  <header class="header">
+  <header class="header">  
     <nav class="menu">
       <router-link class="menu__link mr-2" to="/">Home</router-link>
       <router-link class="menu__link" to="/recent">Pictures</router-link>
     </nav>
-    <div class="header__username">
+    <div class="header-right-side">
+      <div class="header__username">
       {{ store.email }}
     </div>
+    <signOutBtn></signOutBtn>
+    </div>
+  
   </header>
 </template>
 
 <script setup lang="ts">
 import { useStore } from "@/store/index";
+import signOutBtn from '@/components/signOutBtn.vue';
 
 const store = useStore();
+
 </script>
 
 <style lang="scss" scoped>
+.header-right-side {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 header {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  width: 90%;
+  width: 100%;
 }
 </style>
