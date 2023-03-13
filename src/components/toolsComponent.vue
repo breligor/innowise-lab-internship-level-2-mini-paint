adfec5174cd2675120b2ae40160e02c56b1e1809
Igor Brel <igorbrel1983@gmail.com>
2023-03-13 14:24:17 +0300

    add tools component

---
+++ src/components/toolsComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home box is-flex is-justify-content-center">
+    <div class="tools is-flex is-flex-direction-column mr-4">
+      <base-button class="button is-danger mb-2" @Click="store.clearCanvas"
+        >clear</base-button
+      >
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BaseButton from "./base/BaseButton.vue";
+import { useCanvasFieldStore } from "@/store/canvasFieldStore";
+const store = useCanvasFieldStore();
+</script>
+
+<style scoped></style>
