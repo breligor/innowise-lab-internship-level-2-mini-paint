30a940a185d7a384f63e532c4a5616ec851ab6a0
Igor Brel <igorbrel1983@gmail.com>
2023-03-14 13:14:37 +0300

    add data and methods for tools component

--- src/store/canvasFieldStore.ts
+++ src/store/canvasFieldStore.ts
@@ -1,19 +1,24 @@
 import { defineStore } from "pinia";
-
+import { colors } from "@/store/data";
 export const useCanvasFieldStore = defineStore({
   id: "canvasStore",
 
-  state: () => ({  
-    canvas: null, 
+  state: () => ({
+    canvas: null,
     context: null,
-    size: "",
+    size: "1",
     mouseEvent: "up",
     tool: "brush",
-    x:0,
-    y:0,
+    x: 0,
+    y: 0,
+    color: "#000000",
+    isShaped: false,
   }),
 
-  getters: {},
+  getters: {
+    brushSize: (state) => +state.size,
+    colors: () => colors,
+  },
 
   actions: {
     mouseUp() {
@@ -21,6 +26,24 @@ export const useCanvasFieldStore = defineStore({
     },
     clearCanvas() {
       this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.size = "1";
+      this.color = "#000000";
+    },
+    defaultColorIsBlack() {
+      this.color = "#000000";
+    },
+    eraser() {
+      this.color = "#ffffff";
+      this.size = "15";
+    },
+    brush() {
+      this.isShaped = false;
+    },
+    showHideShapes() {
+      this.isShaped = !this.isShaped;
+    },
+    setColor(color: string) {
+      this.color = color;
     },
   },
 });
